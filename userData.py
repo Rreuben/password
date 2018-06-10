f2510fd10585d8385340a6824d5c83c29030cc23
Rreuben <jreuben07@gmail.com>
2018-06-10 21:13:56 +0300

    Set class value for the user data

---
+++ userData.py
@@ -0,0 +1,12 @@
+class userData:
+
+    '''
+    The class for the user accounts
+    '''
+
+    create_account = []
+
+    def __init__( self, username, email, password, confirmPassword ) :
+
+        '''
+        The class
