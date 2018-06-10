d7f209396d2230d8a31b3284b94235c107a18725
Rreuben <jreuben07@gmail.com>
2018-06-10 23:18:16 +0300

    Create class for the password generator

--- passGen.py
+++ passGen.py
@@ -1,36 +1,39 @@
-import string, random
+class passGen :
 
+    import string, random
 
-print( 'How many digits would you like your password to have? (From 9 to 15)' )
+    def generator( passcode ) :
 
-num = input()
+        print( 'How many digits would you like your password to have? (From 9 to 15)' )
 
-def generatePassword( passwrd ) :
-    
-    '''
-    The password generator
-    '''
+        num = input()
 
-    password = str('')
+        def generatePassword( passwrd ) :
+            
+            '''
+            The password generator
+            '''
 
-    for x in range( passwrd ) :
-        x = random.randint( 0, 94 ) 
-        password += string.printable[ x ]
-    return password
+            password = str('')
 
-if num == '9' :
-    print( 'Your new password is : ' + generatePassword( 9 ) )
-elif num == '10' :
-    print( 'Your new password is : ' + generatePassword( 10 ) )
-elif num == '11' :
-    print( 'Your new password is : ' + generatePassword( 11 ) )
-elif num == '12' :
-    print( 'Your new password is : ' + generatePassword( 12 ) )
-elif num == '13' :
-    print( 'Your new password is : ' + generatePassword( 13 ) )
-elif num == '14' :
-    print( 'Your new password is : ' + generatePassword( 14 ) )
-elif num == '15' :
-    print( 'Your new password is : ' + generatePassword( 15) )
-else :
-    print( 'Please stick to the given parameters for now. Thanks :)' )
+            for x in range( passwrd ) :
+                x = random.randint( 0, 94 ) 
+                password += string.printable[ x ]
+            return password
+
+        if num == '9' :
+            print( 'Your new password is : ' + generatePassword( 9 ) )
+        elif num == '10' :
+            print( 'Your new password is : ' + generatePassword( 10 ) )
+        elif num == '11' :
+            print( 'Your new password is : ' + generatePassword( 11 ) )
+        elif num == '12' :
+            print( 'Your new password is : ' + generatePassword( 12 ) )
+        elif num == '13' :
+            print( 'Your new password is : ' + generatePassword( 13 ) )
+        elif num == '14' :
+            print( 'Your new password is : ' + generatePassword( 14 ) )
+        elif num == '15' :
+            print( 'Your new password is : ' + generatePassword( 15) )
+        else :
+            print( 'Please stick to the given parameters for now. Thanks :)' )
