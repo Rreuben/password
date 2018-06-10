b074da9edc7b3ed41e6f53bc67fb92296a199cb5
Rreuben <jreuben07@gmail.com>
2018-06-10 23:42:32 +0300

    Import appropriate classes and modules

--- passwordLocker.py
+++ passwordLocker.py
@@ -1,46 +1,51 @@
-import passGen
+import string, random, 
+from userData import
+from credentialData import credentialsData
+from passGen import passwordGen
 
-class main :
-    
-    def menu() :
 
-        '''
-        The main menu for the program
-        '''
-        print('\n')
-        print( 'Welcome to Pass Manager!' )
-        print( 'The program that stores all your accounts' )
+def menu() :
 
-        while True :
-            print( "Type 'su' to create an account; type 'li' to login to existing account."  )
-            account_logIn = input() .lower()
+    '''
+    The main menu for the program
+    '''
+    print('\n')
+    print( 'Welcome to Pass Manager!' )
+    print( 'The program that stores all your accounts' )
 
-            if account_logIn == 'SU' :
-                print( 'What\'s your first name?' )
-                firstName = input()
-                print( '\n' )
+    while True :
+        print( "Type 'su' to create an account; type 'li' to login to existing account."  )
+        account_logIn = input() .lower()
 
-                print( 'And your last name?' )
-                lastName = input()
-                print( '\n' ) 
+        if account_logIn == 'SU' :
+            print( 'What\'s your first name?' )
+            firstName = input()
+            print( '\n' )
+
+            print( 'And your last name?' )
+            lastName = input()
+            print( '\n' ) 
                 
-                print( 'Your username :' )
-                userName = input()
-                print( '\n' )
+            print( 'Your username :' )
+            userName = input()
+            print( '\n' )
 
-                print( 'And your email :' )
-                emails = input()
-                print('\n')
+            print( 'And your email :' )
+            emails = input()
+            print('\n')
 
-                print( "To create a password, type 'c'; to generate a password, type 'g'" )
-                pass_choice = input()
+            print( "To create a password, type 'c'; to generate a password, type 'g'" )
+            pass_choice = input()
 
-                while True :
-                    if pass_choice == 'c' :
-                        print( 'Your desired password :' )
-                        password = input()
-                        break
-                    elif pass_choice == 'g' :
-                        password = generate( self )
-                        print( '\n' )
-                        break
+            while True :
+                if pass_choice == 'c' :
+                    print( 'Your desired password :' )
+                    password = input()
+                    break
+                elif pass_choice == 'g' :
+                    print( generate() )
+                    break
+                else :
+                    print( "Please type 'c' to create a password or 'g' to generate one." )
+                    break
+                
