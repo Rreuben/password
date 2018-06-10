496ac9a0a7b8684752a62327cd6ef869d21afae6
Rreuben <jreuben07@gmail.com>
2018-06-10 23:30:11 +0300

    Define log in and sign up skeleton

---
+++ passwordLocker.py
@@ -0,0 +1,46 @@
+import passGen
+
+class main :
+    
+    def menu() :
+
+        '''
+        The main menu for the program
+        '''
+        print('\n')
+        print( 'Welcome to Pass Manager!' )
+        print( 'The program that stores all your accounts' )
+
+        while True :
+            print( "Type 'su' to create an account; type 'li' to login to existing account."  )
+            account_logIn = input() .lower()
+
+            if account_logIn == 'SU' :
+                print( 'What\'s your first name?' )
+                firstName = input()
+                print( '\n' )
+
+                print( 'And your last name?' )
+                lastName = input()
+                print( '\n' ) 
+                
+                print( 'Your username :' )
+                userName = input()
+                print( '\n' )
+
+                print( 'And your email :' )
+                emails = input()
+                print('\n')
+
+                print( "To create a password, type 'c'; to generate a password, type 'g'" )
+                pass_choice = input()
+
+                while True :
+                    if pass_choice == 'c' :
+                        print( 'Your desired password :' )
+                        password = input()
+                        break
+                    elif pass_choice == 'g' :
+                        password = generate( self )
+                        print( '\n' )
+                        break
